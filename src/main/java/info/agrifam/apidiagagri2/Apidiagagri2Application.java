35c4b3eca5bc46d309ff445988a12d2b216046a4
alexsoares60 <alex_soares60@hotmail.com>
2023-04-28 09:18:33 -0300

    Initial commit

---
+++ src/main/java/info/agrifam/apidiagagri2/Apidiagagri2Application.java
@@ -0,0 +1,13 @@
+package info.agrifam.apidiagagri2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apidiagagri2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apidiagagri2Application.class, args);
+	}
+
+}
---
+++ src/test/java/info/agrifam/apidiagagri2/Apidiagagri2ApplicationTests.java
@@ -0,0 +1,13 @@
+package info.agrifam.apidiagagri2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apidiagagri2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
